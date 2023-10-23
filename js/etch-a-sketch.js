8c751943d25cfe3046e39771b5570ea1ef84bdcc
Roc327 <nate_van@hotmail.com>
2023-10-23 04:07:36 -0400

    wrote javascript to generate a grid of divs and place in grid_container div

---
+++ js/etch-a-sketch.js
@@ -0,0 +1,22 @@
+function createColumn(size) {
+  for (let i = 0; i < size; i++) {
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("column");
+    newDiv.style.width = 600 / size + "px";
+    document.getElementById("grid_container").appendChild(newDiv);
+    createRow(size, i);
+  }
+}
+
+function createRow(size, i) {
+  for (let j = 0; j < size; j++) {
+    let newRow = document.createElement("div");
+    newRow.textContent = `${i + 1}x${j + 1}`;
+    newRow.classList.add("cell-row");
+    newRow.style.height = 600 / size + "px";
+    let columns = document.querySelectorAll(".column");
+    columns[i].appendChild(newRow);
+  }
+}
+
+createColumn(16);
