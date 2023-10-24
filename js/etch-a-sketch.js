8b287c4ca45e7663f19bda2c8a61af5746e69f2a
Roc327 <nate_van@hotmail.com>
2023-10-24 01:21:17 -0400

    Finish coding completing basic requirements for assignment, have not updated extra credit yet.

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -2,7 +2,6 @@ function createColumn(size) {
   for (let i = 0; i < size; i++) {
     let newDiv = document.createElement("div");
     newDiv.classList.add("column");
-    // newDiv.style.width = 600 / size + "px";
     document.getElementById("grid_container").appendChild(newDiv);
     createRow(size, i);
   }
@@ -11,13 +10,45 @@ function createColumn(size) {
 function createRow(size, i) {
   for (let j = 0; j < size; j++) {
     let newRow = document.createElement("div");
-    newRow.textContent = `${i + 1}x${j + 1}`;
+    // newRow.textContent = `${i + 1}x${j + 1}`;
     newRow.classList.add("cell-row");
     newRow.setAttribute("id", `cell-${i}x${j}`);
-    // newRow.style.height = 600 / size + "px";
+    newRow.setAttribute("onmouseover", "changeColor(this)");
     let columns = document.querySelectorAll(".column");
     columns[i].appendChild(newRow);
   }
+}
+
+function changeColor(obj) {
+  obj.style.backgroundColor = "black";
+}
+
+function clearDivs() {
+  const colElement = document.querySelectorAll(".column");
+  const rowElement = document.querySelectorAll("cell-row");
+  for (const element of rowElement) {
+    element.remove();
+  }
+  for (const element of colElement) {
+    element.remove();
+  }
+}
+
+function clearClick() {
+  let cells = document.querySelectorAll(".cell-row");
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].style.backgroundColor = "#e5e7eb";
+  }
+}
+
+function handleSquaresButton() {
+  clearDivs();
+  let squares = prompt("How many squares in each row and column: ");
+  if (squares > 100) {
+    squares = prompt("Must be 100 or less, please renter an amount: ");
+  }
+  createColumn(squares);
+  //setHeight();
 }
 
 createColumn(16);
