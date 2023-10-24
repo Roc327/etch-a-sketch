6039c28e402421933849c8476b3314121e32205a
Roc327 <nate_van@hotmail.com>
2023-10-23 20:41:38 -0400

    polished up js for grid also added text in div to help visually while editing, added css to display divs in a grid, added in js to add id names to each cell

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -2,7 +2,7 @@ function createColumn(size) {
   for (let i = 0; i < size; i++) {
     let newDiv = document.createElement("div");
     newDiv.classList.add("column");
-    newDiv.style.width = 600 / size + "px";
+    // newDiv.style.width = 600 / size + "px";
     document.getElementById("grid_container").appendChild(newDiv);
     createRow(size, i);
   }
@@ -13,7 +13,8 @@ function createRow(size, i) {
     let newRow = document.createElement("div");
     newRow.textContent = `${i + 1}x${j + 1}`;
     newRow.classList.add("cell-row");
-    newRow.style.height = 600 / size + "px";
+    newRow.setAttribute("id", `cell-${i}x${j}`);
+    // newRow.style.height = 600 / size + "px";
     let columns = document.querySelectorAll(".column");
     columns[i].appendChild(newRow);
   }
